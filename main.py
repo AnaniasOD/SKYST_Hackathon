3c0601417ee9b7182b4cfb88fa9c9753df870183
Hijetsurgery <131605667+Hijetsurgery@users.noreply.github.com>
2024-03-25 15:57:03 +0900

    ptb->main으로 html 넘어가도록 수정

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import uvicorn
 
+html_list = []
+
 app = FastAPI()
 
 @app.post('/upload')
@@ -16,16 +18,16 @@ async def upload(file: UploadFile = File(...)):
         empty_day = []
         df = pd.read_csv(file.file)
         for i in range(len(df)):
-            if df_bro["일정"][i] == 'x' & df_dad["일정"][i] == 'x' & df_mom["일정"][i] == 'x' & df["일정"][i] == 'x':
+            if df_bro["일정"][i] == 'x' and df_dad["일정"][i] == 'x' and df_mom["일정"][i] == 'x' and df["일정"][i] == 'x':
                 empty_day.append(df["날짜"][i])
             else:
                 continue
         print(empty_day[:3])
         global empty
         empty = empty_day[:3]
+        print(empty)
         return empty_day[:3]
 
-html_list = []
 
 @app.post("/submit_radio")
 async def submit_radio(choice: str = Form(...)):
@@ -49,6 +51,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*", "http://localhost:5500"],
+    allow_origins=["*", "http://localhost:8000"],
 )
 
